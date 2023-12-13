1070893915058df855e540495d85f0359ad40fa7
Niels Freier <freier.niels@bcg.com>
2023-12-13 16:21:21 +0100

    adding group

---
+++ groups.tf
@@ -0,0 +1,7 @@
+data "azuread_client_config" "current" {}
+
+resource "azuread_group" "test" {
+  display_name     = "test"
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
+}

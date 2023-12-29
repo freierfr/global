1c97961ef3bfa26fb744612e61acb0ea34d96b34
Niels Freier <freier.niels@bcg.com>
2023-12-29 18:42:29 +0100

    fix

--- marvin.tf
+++ marvin.tf
@@ -91,10 +91,6 @@ resource "azurerm_key_vault_access_policy" "marvin_dev_keyvault_access_policy" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azuread_client_config.current.object_id
 
-  #   key_permissions = [
-  #     "Get",
-  #   ]
-
   secret_permissions = [
     "Get", "List", "Delete", "Recover", "Backup", "Restore", "Set",
   ]
